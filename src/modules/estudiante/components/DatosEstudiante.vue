0be1dcceee2d010dda55955c96a1ead383b24bad
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-07-29 10:51:46 -0500

    Taller 35: Consumo de API

---
+++ src/modules/estudiante/components/DatosEstudiante.vue
@@ -0,0 +1,24 @@
+<template>
+  <input v-model="cedula" type="text">
+  <button @click="consultarEstudiante">Consultar</button>
+</template>
+
+<script>
+import {obtenerEstudianteFachada} from "../helpers/EstudianteClient.js"
+export default {
+    data(){
+        return{
+            cedula:null
+        }
+    }, 
+    methods:{
+       async consultarEstudiante(){
+           await obtenerEstudianteFachada(this.cedula);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
